54d320ab06b079fa372aa2b51579826829a457c9
Lucas Gregorio <luks.27@hotmail.com>
2022-10-17 14:24:46 -0300

    add botao na aba projetos com js

---
+++ main.js
@@ -0,0 +1,105 @@
+const botao1 = document.querySelector("#button1")
+const botao2 = document.querySelector("#button2")
+const botao3 = document.querySelector("#button3")
+const botao4 = document.querySelector("#button4")
+const botao5 = document.querySelector("#button5")
+const botao6 = document.querySelector("#button6")
+const botao7 = document.querySelector("#button7")
+const botao8 = document.querySelector("#button8")
+
+const div1 = document.querySelector(".projeto-div-informacao1")
+const div2 = document.querySelector(".projeto-div-informacao2")
+const div3 = document.querySelector(".projeto-div-informacao3")
+const div4 = document.querySelector(".projeto-div-informacao4")
+const div5 = document.querySelector(".projeto-div-informacao5")
+const div6 = document.querySelector(".projeto-div-informacao6")
+const div7 = document.querySelector(".projeto-div-informacao7")
+const div8 = document.querySelector(".projeto-div-informacao8")
+
+botao1.onclick = clickBotao1
+
+function clickBotao1 (){
+
+    if(div1.style.display === 'block'){
+        div1.style.display = 'none'
+    } else {
+        div1.style.display = 'block'
+    }
+}
+
+botao2.onclick = clickBotao2
+
+function clickBotao2 (){
+
+    if(div2.style.display === 'block'){
+        div2.style.display = 'none'
+    } else {
+        div2.style.display = 'block'
+    }
+}
+
+botao3.onclick = clickBotao3
+
+function clickBotao3 (){
+
+    if(div3.style.display === 'block'){
+        div3.style.display = 'none'
+    } else {
+        div3.style.display = 'block'
+    }
+}
+
+botao4.onclick = clickBotao4
+
+function clickBotao4 (){
+
+    if(div4.style.display === 'block'){
+        div2.style.display = 'none'
+    } else {
+        div4.style.display = 'block'
+    }
+}
+
+botao5.onclick = clickBotao5
+
+function clickBotao5 (){
+
+    if(div5.style.display === 'block'){
+        div5.style.display = 'none'
+    } else {
+        div5.style.display = 'block'
+    }
+}
+
+botao6.onclick = clickBotao6
+
+function clickBotao6 (){
+
+    if(div6.style.display === 'block'){
+        div6.style.display = 'none'
+    } else {
+        div6.style.display = 'block'
+    }
+}
+
+botao7.onclick = clickBotao7
+
+function clickBotao7 (){
+
+    if(div7.style.display === 'block'){
+        div7.style.display = 'none'
+    } else {
+        div7.style.display = 'block'
+    }
+}
+
+botao8.onclick = clickBotao8
+
+function clickBotao8 (){
+
+    if(div8.style.display === 'block'){
+        div8.style.display = 'none'
+    } else {
+        div8.style.display = 'block'
+    }
+}
